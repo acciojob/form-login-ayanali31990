7b2c0fac114ee8f07c4c503296d771e56f837ff8
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-10-04 11:17:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+const fname = document.querySelector('#fname').value
+const lname = document.querySelector('#lname').value
+		alert(`${fname} ${lname}`)
+	
 }
